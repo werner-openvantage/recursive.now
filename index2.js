a1e4b3c49c439701073300e241d59bf19c0833d0
Werner Potgieter <werner@openvantage.co.za>
2025-09-17 09:52:47 +0200

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,8 @@
+const string = 'this.is.a.test.string';
+const value = '2';
+
+// Create a function that will build a object from the string and set the value of the last key to the value
+const buildObject = (string, value) => {
+
+};
+console.log(buildObject(string, value));
